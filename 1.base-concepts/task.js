38acb81c3084f322dd9e884721b0a585d589ae32
abidat <abirubyjane@gmail.com>
2024-04-08 22:40:49 +0300

    Задание 1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,27 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
-  return arr;
-}
+  let D = b ** 2 - 4 * a * c;
+   
+  if(D < 0) {
+    return arr;
+  } 
 
+  if(D === 0) {  
+    arr.push((-b / 2 * a));
+    return arr;
+  }
+
+  else if(D > 0){
+  let rootEquation = [];
+  rootEquation.push((-b + Math.sqrt(D)) / (2 * a));
+  rootEquation.push((-b - Math.sqrt(D)) / (2 * a));
+  arr = rootEquation;
+};
+ return arr;
+};
+
+solveEquation(2, 4, 1);
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   
 }
