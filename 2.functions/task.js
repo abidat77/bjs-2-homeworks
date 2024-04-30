96b18c836facd806fce2d081a11fe8ac21b55386
abidat <abirubyjane@gmail.com>
2024-05-01 00:08:39 +0300

    Функции. Задача 1

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,7 +1,20 @@
 function getArrayParams(...arr) {
+  let min = Infinity;
+  let max = -Infinity;
+  let sum = 0;
+  for(let i = 0; i < arr.length; i++) {
+    let number = arr[i];
+    sum += number;
+    if (max < number) max = number;
+    if (min > number) min = number; 
+  }
+  let avg = +(sum / arr.length).toFixed(2); 
+  return { min: min, max: max, avg: avg }
+  }
+  
+getArrayParams(-99, 99, 10);
 
-  return { min: min, max: max, avg: avg };
-}
+
 
 function summElementsWorker(...arr) {
 
