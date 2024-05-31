11fb557acb1488478fe8171c1417bd3b51616f18
abidat <abirubyjane@gmail.com>
2024-05-31 23:59:29 +0300

    ДЗ Объекты

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -6,17 +6,45 @@ function Student(name, gender, age) {
 }
 
 Student.prototype.setSubject = function (subjectName) {
+  this.subject = subjectName;
   
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+  if (this.hasOwnProperty('marks')) {
+    this.marks.push(...marks);
+  }
+  else {
+    return 0;
+  }
 }
 
 Student.prototype.getAverage = function () {
-  
+  if (this.marks.length === 0) {
+    return 0;
+  }
+
+  let sum = this.marks.reduce((total, mark) => total + mark, 0);
+  return sum / this.marks.length;
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.subject;
+  delete this.marks;
+  Object.defineProperty(this, 'excluded', {
+    value: 'прогулы',
+  });
 }
+
+
+let student1 = new Student("Василиса", "женский", 19);
+student1.setSubject("Algebra");
+console.log(student1.getAverage()); 
+student1.addMarks(4, 5, 4, 5);
+console.log(student1.getAverage());
+console.log(student1);
+
+let student2 = new Student("Артём", "мужской", 25);
+student2.setSubject("Geometry");
+student2.exclude('плохая учёба');
+console.log(student2);
