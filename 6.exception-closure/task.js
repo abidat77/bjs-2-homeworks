b8663b329815f1003dc63f12b91e2bcfa7a8c803
abidat <abirubyjane@gmail.com>
2025-01-23 16:05:34 +0300

    ДЗ- Исключения и замыкания полностью

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -14,7 +14,51 @@ function validateCount(x) {
     try {
         return parseCount(x);
     } catch (error) {
-        return 'Невалидное значение';
+        return error;
     }
 }
 validateCount(numPurchases);
+
+
+class Triangle {
+    constructor(a, b, c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+
+        if (this.a + this.b <= this.c || this.a + this.c <= this.b || this.b + this.c <= this.a) {
+            throw new Error("Треугольник с такими сторонами не существует");
+        }
+    }
+
+    get perimeter() {
+        return this.a + this.b + this.c;
+    }
+
+    get area() {
+        const s = this.perimeter / 2; // Полупериметр
+        return Math.sqrt(s * (s - this.a) * (s - this.b) * (s - this.c)).toFixed(3);
+    }
+}
+
+function getTriangle(a, b, c) {
+    try {
+        return new Triangle(a, b, c);
+    } 
+    catch (error) {
+        return {
+            get area() {
+                return "Ошибка! Треугольник не существует";
+            },
+            get perimeter() {
+                return "Ошибка! Треугольник не существует";
+            }
+        };
+    }
+}
+
+
+const triangle1 = getTriangle(3, 4, 5);
+console.log(triangle1.perimeter); 
+console.log(triangle1.area);
+
