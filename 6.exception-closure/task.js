9cafad530c31825933a573bb3bfd4f72c57d6cc3
abidat <abirubyjane@gmail.com>
2025-01-23 06:53:09 +0300

    ДЗ-Обработка исключений и замыкания

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,20 @@
-﻿
+﻿let numPurchases = prompt();
+
+function parseCount(x) {
+  let y = parseFloat(x);
+   if(isNaN(y)) {
+        throw new Error('Невалидное значение');
+   }
+    else {
+        return y;
+    }
+}
+
+function validateCount(x) {
+    try {
+        return parseCount(x);
+    } catch (error) {
+        return 'Невалидное значение';
+    }
+}
+validateCount(numPurchases);
